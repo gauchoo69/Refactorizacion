0f75dd09cdf7f742ea67b9830bbeb8c51e0a7665
gael <gmarcelinodocio@danielcastelao.org>
2025-04-09 22:32:42 +0200

    Segundo commit (Ventajasmaximas).

--- src/Main.java
+++ src/Main.java
@@ -22,27 +22,33 @@ public class Main {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                score = Ventajasmaximas(m_score1, m_score2, i, score);
             }
 
         }
     return score;
+    }
+
+    private static String Ventajasmaximas(int m_score1, int m_score2, int i, String score) {
+        int tempScore;
+        if (i ==1) tempScore = m_score1;
+        else { score +="-"; tempScore = m_score2;}
+        switch(tempScore)
+        {
+            case 0:
+                score +="Love";
+                break;
+            case 1:
+                score +="Fifteen";
+                break;
+            case 2:
+                score +="Thirty";
+                break;
+            case 3:
+                score +="Forty";
+                break;
+        }
+        return score;
     }
 
     private static String PuntuacionEmpate(int m_score1) {
